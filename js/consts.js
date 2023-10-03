ddfa7e6dc185c8c9c5c343eabc7f665fbab43d58
hyeri <cos850@naver.com>
2023-10-03 16:30:08 +0900

    mbti 적용

--- js/consts.js
+++ js/consts.js
@@ -1,5 +1,100 @@
-const testList = [
-    {question: `역시 휴일엔 집에 있는게 힐링이죠?`, choice1: '맞아요!', choice2: '엥 나가야죠~'},
-    {question: `친구와 카페에서 만날 약속을 잡았는데, 무슨카페를 갈까요?`, choice1: '항상 가던 편안한 카페', choice2: '새로 생긴 SNS 핫플'},
-    {question: `카페로 가던 중 보인 꽃집에서 꽃을 살까 말까 망설여져요. 이유는?`, choice1: '꽃말의 의미가 너무 좋았어요', choice2: '꽃이 생각보다 저렴해서 좋았어요'},
+const tests = [
+    {type: 'EI', question: `외향형(E) vs 내향형(I) 테스트 질문`, answers: [{type: 'I', text: 'Introversion'}, {type: 'E', text: 'Extroversion'}]},
+    {type: 'SN', question: `감각형(S) vs 직관형(N) 테스트 질문`, answers: [{type: 'S', text: 'Sensing'}, {type: 'N', text: 'iNtuition'}]},
+    {type: 'TF', question: `사고형(T) vs 감정형(F) 테스트 질문`, answers: [{type: 'F', text: 'Feeling'}, {type: 'T', text:'Thinking'}]},
+    {type: 'JP', question: `판단형(J) vs 인식형(P) 테스트 질문`, answers: [{type: 'P', text: 'Perceiving'}, {type: 'J', text: 'Judging'}]}
 ];
+
+const results = [
+    {
+        type: 'ISTJ',
+        menu: 'ISTJ 메뉴',
+        desc: 'ISTJ 메뉴 및 성격 특징의 설명 공간. 메뉴 및 성격 특징의 설명 공간. 메뉴 및 성격 특징의 설명 공간. 메뉴 및 성격 특징의 설명 공간. 메뉴 및 성격 특징의 설명 공간. '
+    },
+    {
+        type: 'ISFJ',
+        menu: 'ISFJ Menu',
+        desc: 'ISFJ 메뉴 및 성격 특징의 설명 공간. 메뉴 및 성격 특징의 설명 공간. 메뉴 및 성격 특징의 설명 공간. 메뉴 및 성격 특징의 설명 공간. 메뉴 및 성격 특징의 설명 공간. '
+    },
+    {
+        type: 'INFJ',
+        menu: 'INFJ Menu',
+        desc: 'INFJ 메뉴 및 성격 특징의 설명 공간. 메뉴 및 성격 특징의 설명 공간. 메뉴 및 성격 특징의 설명 공간. 메뉴 및 성격 특징의 설명 공간. 메뉴 및 성격 특징의 설명 공간. '
+    },
+    {
+        type: 'INTJ',
+        menu: 'INTJ Menu',
+        desc: 'INTJ 메뉴 및 성격 특징의 설명 공간. 메뉴 및 성격 특징의 설명 공간. 메뉴 및 성격 특징의 설명 공간. 메뉴 및 성격 특징의 설명 공간. 메뉴 및 성격 특징의 설명 공간. '
+    },
+    {
+        type: 'ISTP',
+        menu: 'ISTP 메뉴',
+        desc: 'ISTP 메뉴 및 성격 특징의 설명 공간. 메뉴 및 성격 특징의 설명 공간. 메뉴 및 성격 특징의 설명 공간. 메뉴 및 성격 특징의 설명 공간. 메뉴 및 성격 특징의 설명 공간. '
+    },
+    {
+        type: 'ISFP',
+        menu: 'ISFP Menu',
+        desc: 'ISFP 메뉴 및 성격 특징의 설명 공간. 메뉴 및 성격 특징의 설명 공간. 메뉴 및 성격 특징의 설명 공간. 메뉴 및 성격 특징의 설명 공간. 메뉴 및 성격 특징의 설명 공간. '
+    },
+    {
+        type: 'INFP',
+        menu: 'INFP Menu',
+        desc: 'INFP 메뉴 및 성격 특징의 설명 공간. 메뉴 및 성격 특징의 설명 공간. 메뉴 및 성격 특징의 설명 공간. 메뉴 및 성격 특징의 설명 공간. 메뉴 및 성격 특징의 설명 공간. '
+    },
+    {
+        type: 'INTP',
+        menu: 'INTP Menu',
+        desc: 'INTP 메뉴 및 성격 특징의 설명 공간. 메뉴 및 성격 특징의 설명 공간. 메뉴 및 성격 특징의 설명 공간. 메뉴 및 성격 특징의 설명 공간. 메뉴 및 성격 특징의 설명 공간. '
+    },
+    {
+        type: 'ESTP',
+        menu: 'ESTP 메뉴',
+        desc: 'ESTP 메뉴 및 성격 특징의 설명 공간. 메뉴 및 성격 특징의 설명 공간. 메뉴 및 성격 특징의 설명 공간. 메뉴 및 성격 특징의 설명 공간. 메뉴 및 성격 특징의 설명 공간. '
+    },
+    {
+        type: 'ESFP',
+        menu: 'ESFP Menu',
+        desc: 'ESFP 메뉴 및 성격 특징의 설명 공간. 메뉴 및 성격 특징의 설명 공간. 메뉴 및 성격 특징의 설명 공간. 메뉴 및 성격 특징의 설명 공간. 메뉴 및 성격 특징의 설명 공간. '
+    },
+    {
+        type: 'ENFP',
+        menu: 'ENFP Menu',
+        desc: 'ENFP 메뉴 및 성격 특징의 설명 공간. 메뉴 및 성격 특징의 설명 공간. 메뉴 및 성격 특징의 설명 공간. 메뉴 및 성격 특징의 설명 공간. 메뉴 및 성격 특징의 설명 공간. '
+    },
+    {
+        type: 'ENTP',
+        menu: 'ENTP Menu',
+        desc: 'ENTP 메뉴 및 성격 특징의 설명 공간. 메뉴 및 성격 특징의 설명 공간. 메뉴 및 성격 특징의 설명 공간. 메뉴 및 성격 특징의 설명 공간. 메뉴 및 성격 특징의 설명 공간. '
+    },
+    {
+        type: 'ESTJ',
+        menu: 'ESTJ 메뉴',
+        desc: 'ESTJ 메뉴 및 성격 특징의 설명 공간. 메뉴 및 성격 특징의 설명 공간. 메뉴 및 성격 특징의 설명 공간. 메뉴 및 성격 특징의 설명 공간. 메뉴 및 성격 특징의 설명 공간. '
+    },
+    {
+        type: 'ESFJ',
+        menu: 'ESFJ Menu',
+        desc: 'ESFJ 메뉴 및 성격 특징의 설명 공간. 메뉴 및 성격 특징의 설명 공간. 메뉴 및 성격 특징의 설명 공간. 메뉴 및 성격 특징의 설명 공간. 메뉴 및 성격 특징의 설명 공간. '
+    },
+    {
+        type: 'ENFJ',
+        menu: 'ENFJ Menu',
+        desc: 'ENFJ 메뉴 및 성격 특징의 설명 공간. 메뉴 및 성격 특징의 설명 공간. 메뉴 및 성격 특징의 설명 공간. 메뉴 및 성격 특징의 설명 공간. 메뉴 및 성격 특징의 설명 공간. '
+    },
+    {
+        type: 'ENTJ',
+        menu: 'ENTJ Menu',
+        desc: 'ENTJ 메뉴 및 성격 특징의 설명 공간. 메뉴 및 성격 특징의 설명 공간. 메뉴 및 성격 특징의 설명 공간. 메뉴 및 성격 특징의 설명 공간. 메뉴 및 성격 특징의 설명 공간. '
+    }
+]
+
+function findResult(mbti){
+    const resultType = (mbti.E > mbti.I ? 'E' : 'I') 
+        + (mbti.N > mbti.S ? 'N' : 'S')
+        + (mbti.F > mbti.T ? 'F' : 'T')
+        + (mbti.J > mbti.P ? 'J' : 'P');
+
+        console.log('result: ' + resultType);
+    
+    return results.find(ele=>ele.type === resultType);
+}
--- js/index.js
+++ js/index.js
@@ -6,8 +6,7 @@ const $result = $root.find('#result');
 const $question = $test.find('#question');
 const $choiceGroup = $test.find('#choice-group');
 
-
-let answers = [];
+const mbti = {E: 0, I: 0, N: 0, S: 0, T: 0, F: 0, J: 0, P: 0};
 let current = -1;
 
 function showMain(){
@@ -27,39 +26,31 @@ function showResult(){
     $test.hide();
     $main.hide();
 
-    // TODO_HR : 결과 가져오기
-    const result = {
-        menu: '아메리카노',
-        desc: '당신은 ~~~~ 성향이시군요? 이렇게 멋진 당신에게는 아메리카노가 어울려요.'
-    };
+    const result = findResult(mbti);
     
     $result.find('.h1').text(result.menu);
     $result.find('.px-3').text(result.desc);
     $result.show();
 }
 
-function choice(selectedValue){
-    answers[current] = {
-        Q: current,
-        A: selectedValue
-    };
-
-    console.dir(answers);
-    
-    next();
-}
-
 function next(){
-    current++;
-
-    if(current === testList.length) { // last question
+    if(++current > tests.length-1) {    // end test
         showResult();
     } else {
-        let testObj = testList[current];
+        const testObj = tests[current];
 
         $question.text(testObj.question);
-        $choiceGroup.children().eq(0).text(testObj.choice1);
-        $choiceGroup.children().eq(1).text(testObj.choice2);
+
+        const $btn0 = $choiceGroup.children().eq(0);
+        const $btn1 = $choiceGroup.children().eq(1);
+
+        $btn0.text(testObj.answers[0].text)
+            .off('click')
+            .on('click', ()=>choice(testObj.answers[0].type));
+        
+        $btn1.text(testObj.answers[1].text)
+            .off('click')
+            .on('click', ()=>choice(testObj.answers[1].type));
     } 
 }
 
@@ -67,4 +58,9 @@ function share(){
     navigator.clipboard.writeText(location.protocol + "//" + location.host).then(()=>{
         alert('복사되었습니다.');
     });
+}
+
+function choice(type){
+    mbti[type]++;
+    next();
 }
